28de3933d56b6bd2c679bfd93c6517903673047f
danueli <danurich12@gmail.com>
2026-03-09 11:36:54 +0800

    added guidelines for connecting

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const { engine } = require('express-handlebars');
 const app = express();
 
 // MongoDB connection
-mongoose.connect('mongodb://enzorosas_db_user:r7BhPh3X2G5yguqu@ac-75jwuwg-shard-00-00.emfl2fn.mongodb.net:27017,ac-75jwuwg-shard-00-01.emfl2fn.mongodb.net:27017,ac-75jwuwg-shard-00-02.emfl2fn.mongodb.net:27017/bakehubdb?ssl=true&replicaSet=atlas-14b2hv-shard-0&authSource=admin&appName=Cluster0')
+// Kindly change the connection string by adding ur username and ps
+// Syntax: 'mongodb://<username>:<password>@ac----
+mongoose.connect('mongodb://danieldabuit_db_user:1234@ac-75jwuwg-shard-00-00.emfl2fn.mongodb.net:27017,ac-75jwuwg-shard-00-01.emfl2fn.mongodb.net:27017,ac-75jwuwg-shard-00-02.emfl2fn.mongodb.net:27017/bakehubdb?ssl=true&replicaSet=atlas-14b2hv-shard-0&authSource=admin&appName=Cluster0')
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
 
