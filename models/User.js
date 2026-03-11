a1bf20c20d98fd8c3328b400030a23f757772484
enzorosas <enzorosas90@gmail.com>
2026-03-11 16:47:15 +0800

    added User and Reviews.js

---
+++ models/User.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  // Personal Information
+  firstName: { type: String, required: true },
+  lastName:  { type: String, required: true },
+  email:     { type: String, required: true, unique: true },
+  phone:     { type: String },
+
+  // Account Details
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+ userType: { type: String, enum: ['customer', 'admin'], default: 'customer' },
+
+
+  // Shipping Address
+  street:   { type: String },
+  city:     { type: String },
+  province: { type: String },
+  zip:      { type: String },
+});
+
+module.exports = mongoose.model('User', userSchema);
