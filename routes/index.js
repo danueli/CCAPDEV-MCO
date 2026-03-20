5e85e3367056398b6ea265a109b23a4a0aa2d892
ethanndaniel <ethan_daniel_magbatoc@dlsu.edu.com.ph>
2026-03-20 21:26:58 +0800

    fixed Cart path

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Product = require('../models/Product');
 const User = require('../models/User'); 
-const Cart = require('/..models/Cart');
+const Cart = require('../models/Cart');
 
 
 router.get('/', (req, res) => {
